57e4be9eec5257373cc24f504baf8151d9de9287
leofk <73722423+leofk@users.noreply.github.com>
2024-04-28 18:57:48 -0700

    basic framework done

--- src/minimesh/core/mohe/mesh_mapping.cpp
+++ src/minimesh/core/mohe/mesh_mapping.cpp
@@ -1,71 +1,35 @@
 #include <minimesh/core/mohe/mesh_mapping.hpp>
 #include <minimesh/core/util/assert.hpp>
+#include <set>
 
 namespace minimesh
 {
 namespace mohe
 {
 
+
 void Mesh_mapping::build_mapping()
 {
 	// inter surface mapping
+	current_mesh = M1;
 
-	// given two meshes, one-to-one vertex correspondance, same connectivity'
-	// m1 - mesh 1 as it were
-	// m1p - mesh 2 on mesh 1s positions
-	// m2 - mesh 2 as it were
-	// m2p - mesh 1 on mesh 2s positions
-
-	// init
-		// maps from m1 -> m2p
-		// maps from m2 -> m1p
-		// intersurface map
-
-	// todo - init
-	/*
-	- we split a vertex on m1, call it v1
-	- we use the same info to split vertex on m2'
-	- now we need to figure out where m2' is on m2 to derive the inter-surface mapping
-	- now split a vertex on m2
-	- we need to check if this split affected any mapping from m1->m2, i.e if faces changed
-	- go to m1' and split vert
-	- compute mapping for vert on m1' is relative to m1
-	- repeat
-	*/
+	
+	
 }
 
-//
-// split the vertex in original mesh according to reverse edge-collapse
-//
-// void Mesh_mapping::vertex_split()
-// {
-	// suppose we split on m1
-	// here we need to check if this split fucked with our previous mappings from m2-> m1
-		// ie. suppose v on m2, there is a map v -> some face in m1, but b/c of the 
-		// split on m1, v might no longer belong to that face. so we need to realize its bary cords
-	// now compute barycentric coordinates of new v (in m1) with respect to its ring
-	// use map to find positions N(v) 
-	// compute v' position on m2 using bary ^^ maybe above can be parrallel	
-	// v' must be contained in of the faces (in m2) in map used to find N(v)
-	// see which of the faces contain v' (via bary coords)
-	// the face with non negative coords is our mapping
-// }
-
-void Mesh_mapping::ISM_iteration(int m)
+void Mesh_mapping::ISM_iteration()
 {
 	// say M1, split vertex. 
 	std::map<int, inter_data> & ISM = ISM_M1;
-	if (m == 1) {
-		Mesh_connectivity & mesh = _m1;
-	}
-	if (m == 2) {
-		Mesh_connectivity & mesh = _m2;
-		ISM = ISM_M2;
-	}
+	if (current_mesh != M1) ISM = ISM_M2;
 
 	// 1. pop from top of collapse queue - HistroyEntry
 
 	//todo adapt akumah
+
+	// here we need to check if this split fucked with our previous mappings from m2-> m1
+	// ie. suppose v on m2, there is a map v -> some face in m1, but b/c of the 
+	// split on m1, v might no longer belong to that face. so we need to realize its bary cords
 
 	// 2. get keptFaces and removedVertex from history Entry
 	std::vector<int> faces; // = historyEntry.keptFaces
@@ -136,13 +100,23 @@ Eigen::Vector3d Mesh_mapping::get_pos_from_inter_data(inter_data data)
 }
 
 //
-// TODO - compute the barycentric coordinates of v_pos wihin each face in faces
+// compute the barycentric coordinates of v_pos wihin each face in faces
 // return the face and coordinates of the face with non-negative bary. coords
 //
 inter_data Mesh_mapping::find_enclosing_face(std::vector<int> faces, Eigen::Vector3d v_pos)
 {
-	std::vector<double> bary; 
-	int f_id;
+	std::set<int> faces_set(faces.begin(), faces.end());
+
+    // Compute barycentric coordinates for each element in the set
+    int f_id;
+	std::vector<double> bary;
+
+  	for (auto it = faces_set.begin(); it != faces_set.end(); ++it) {
+        f_id = *it;
+		bary = compute_bc(f_id, v_pos);
+		if (bary[0] != -1.0) {break;}
+    }
+
 	return inter_data(f_id, bary);
 }
 
@@ -176,35 +150,73 @@ inter_data Mesh_mapping::trivial_map_data(int vid)
 	return inter_data(f_id, bary);
 }
 
+
 //
+// compute the barycentric coordinates of v_pos wihin f_id
+//
+std::vector<double> Mesh_mapping::compute_bc(int f_id, Eigen::Vector3d v_pos)
+{
+	Mesh_connectivity::Face_iterator f = mesh().face_at(f_id);
+	Mesh_connectivity::Half_edge_iterator he = f.half_edge();
+	Mesh_connectivity::Vertex_iterator v = he.origin();
+	std::vector<double> bary;
+
+	std::vector<double> fail;
+	fail.push_back(-1.0);
+
+	std::vector<Eigen::Vector3d> verts;
+	do
+	{
+		Mesh_connectivity::Vertex_iterator v_curr = he.origin();
+		verts.push_back(v_curr.xyz());
+		he = he.next();
+	} while (he.origin().index() != v.index());
+
+    for (const auto& vert : verts) 
+	{
+		double w = get_wij(v_pos, vert, verts);
+		if (w < 0.0) {
+			return fail;
+		}
+		else {
+			bary.push_back(w);
+		}
+	}
+
+	return bary;
+}
+
 // barycentric coordinates (mean-value)
 // the weight of vertex j for i
-//
-// double Mesh_fixed_param::get_wik(int he_index) 
-// {
-// 	// points to i from j
-// 	Mesh_connectivity::Half_edge_iterator he = mesh().half_edge_at(he_index); 
+double Mesh_mapping::get_wij(Eigen::Vector3d i, Eigen::Vector3d j, std::vector<Eigen::Vector3d> &verts) 
+{
+	// Assuming verts always has size 3
+    Eigen::Vector3d p, q;
 
-// 	Mesh_connectivity::Vertex_iterator I = he.dest();
-// 	Mesh_connectivity::Vertex_iterator J = he.origin();  
-// 	Mesh_connectivity::Vertex_iterator P = he.twin().next().dest(); 
-// 	Mesh_connectivity::Vertex_iterator Q = he.next().dest(); 
+    // Find the other two vertices in verts
+    for (const auto& vert : verts) {
+        if (vert != j) {
+            if (p == Eigen::Vector3d::Zero()) {
+                p = vert;
+            } else {
+                q = vert;
+                break;
+            }
+        }
+    }
 
-// 	Eigen::Vector3d I_xyz = I.xyz();
-// 	Eigen::Vector3d J_xyz = J.xyz();
-// 	Eigen::Vector3d P_xyz = P.xyz();
-// 	Eigen::Vector3d Q_xyz = Q.xyz();
+	Eigen::Vector3d IJ = j-i;
+	Eigen::Vector3d IP = p-i;
+	Eigen::Vector3d IQ = q-i;
 
-// 	Eigen::Vector3d IJ = J_xyz-I_xyz;
-// 	Eigen::Vector3d IP = P_xyz-I_xyz;
-// 	Eigen::Vector3d IQ = Q_xyz-I_xyz;
+	double alpha = get_angle(IJ, IP);
+	double beta = get_angle(IJ, IQ);
+	double r = IJ.norm();
 
-// 	double alpha = get_angle(IJ, IP);
-// 	double beta = get_angle(IJ, IQ);
-// 	double r = IJ.norm();
+	return (std::tan(alpha / 2) + std::tan(beta / 2)) / r;
+}
 
-// 	return (std::tan(alpha / 2) + std::tan(beta / 2)) / r;
-// }
+
 
 } // end of mohe
 } // end of minimesh
--- src/minimesh/core/mohe/mesh_mapping.hpp
+++ src/minimesh/core/mohe/mesh_mapping.hpp
@@ -4,6 +4,9 @@
 
 #include <minimesh/core/mohe/mesh_connectivity.hpp>
 
+#define M1 1
+#define M2 2
+
 struct inter_data {
     std::vector<double> bary; 
 	int f_id; 
@@ -19,32 +22,45 @@ class Mesh_mapping
 {
 public:
 	// Trivial constructor
-	Mesh_mapping(Mesh_connectivity & mesh_in): _m(mesh_in) {}
+	Mesh_mapping(Mesh_connectivity & m1, Mesh_connectivity & m2): _m1(m1), _m2(m2) {}
 
 	// Get the underlying mesh
-	Mesh_connectivity & mesh() { return _m; }
-	const Mesh_connectivity & mesh() const { return _m; }
+	Mesh_connectivity & m1() { return _m1; }
+	Mesh_connectivity & m2() { return _m2; }
+	Mesh_connectivity & mesh() {
+		if (current_mesh == M1)
+			return _m1;
+		else
+			return _m2;
+	}
+	// const Mesh_connectivity & mesh() const { return _m; }
 
 	void build_mapping();
 	void init_ISM();
-	void ISM_iteration(int m);
+	void ISM_iteration();
 	inter_data trivial_map_data(int vid);
 	void yep(Mesh_connectivity & mesh_in);
 	inter_data find_enclosing_face(std::vector<int> faces, Eigen::Vector3d v_pos);
 	Eigen::Vector3d get_pos_from_inter_data(inter_data data);
+	std::vector<double> compute_bc(int f_id, Eigen::Vector3d v_pos);
+	double get_wij(Eigen::Vector3d i, Eigen::Vector3d j, std::vector<Eigen::Vector3d> &verts); 
 
 
 private:
 	// pointer to the mesh that we are working on.
-	Mesh_connectivity & _m;
-	// Mesh_connectivity & _m1;
-	// Mesh_connectivity & _m2;
+	// Mesh_connectivity & _m;
+	Mesh_connectivity & _m1;
+	Mesh_connectivity & _m2;
 
     std::map<int, int> mesh_map;
     std::map<int, inter_data> ISM_M1;
     std::map<int, inter_data> ISM_M2;
-
+	int current_mesh;
 };
+
+double get_angle(const Eigen::Vector3d &v1, const Eigen::Vector3d &v2) {
+	return std::acos(v1.dot(v2) / (v1.norm() * v2.norm()));
+}
 
 
 } // end of mohe
